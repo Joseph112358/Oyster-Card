41d55a658f4eaecff5695fdd59df7b5315901e18
Joe <josephshaw1563@gmail.com>
2021-09-27 15:05:53 +0100

    First test passed

--- Projects/OOP/oyster/lib/oyster_card.rb
+++ Projects/OOP/oyster/lib/oyster_card.rb
@@ -1,3 +1,10 @@
 class Oyster_card
 
+def initialize
+@balance = 0
+end
+
+def balance
+return @balance
+end
 end
--- Projects/OOP/oyster/spec/oyster_card_spec.rb
+++ Projects/OOP/oyster/spec/oyster_card_spec.rb
@@ -1,5 +1,8 @@
 require_relative "../lib/oyster_card.rb"
 
 describe Oyster_card do
-
+  it 'checks the balance is zero at initialisation' do
+    
+    expect(subject.balance).to eq 0
+  end
 end
